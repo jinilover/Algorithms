d8ca94c9dd3295b2c89c0cb7d90cbc5f0edace76
jinilover <columbawong@gmail.com>
2019-03-14 15:46:26 +0800

    add floydWarshall

--- src/Graphs.hs
+++ src/Graphs.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE NoImplicitPrelude
-           , RecordWildCards #-}
+           , RecordWildCards
+           , TupleSections #-}
 
 module Graphs where
 
 import Protolude
 import Data.List (nub)
 import qualified Data.Map as M
+import qualified Data.Vector as V
 
 data RoseTree a = Nde a [RoseTree a] deriving Show -- is it n-ary tree can be presented by cofree
 
@@ -118,7 +120,7 @@ dfsGraph src dest graph = maybe [] (\node ->
 findNode :: Eq a => Graph a -> a -> Maybe (Node a)
 findNode = findPoint node
 
-type Edge a = (a, Int) -- (vertex, cost)
+type Edge a = (a, Int)
 
 data Vertex a = Vertex {
     vertex :: a
@@ -170,9 +172,10 @@ shortestPath src dest w
 
         takeMin Nothing newPath = Just newPath
         takeMin path newPath = (\p -> if costs p < costs newPath then p else newPath) <$> path
-        
-        costs = sum . map snd
 
+costs :: [(a, Int)] -> Int
+costs = sum . map snd
+        
 allPaths :: Eq a => a -> a -> Web a -> [[a]]
 allPaths src dest w
   | src == dest = []
@@ -188,3 +191,63 @@ allPaths src dest w
               let (_, newPaths) = checkVertices (visited ++ [edge]) edges paths in
               checkVertices visited eds newPaths
             ) $ findVertex w vertex
+
+type Path = Maybe [(Int, Int)]
+
+type Matrix = V.Vector (V.Vector Path) -- (Index, cost)
+
+-- |
+-- >>> optimumPath 1 3 [(1,2,10),(1,3,20),(1,4,1),(2,3,16),(3,4,4)]
+-- [1,4,3]
+-- 
+-- >>> optimumPath 2 3 [(1,2,10),(1,3,20),(1,4,1),(2,3,16),(3,4,4)]
+-- [2,1,4,3]
+-- 
+-- >>> optimumPath 'a' 'b' [('a', 'b', 12), ('a', 'c', 7), ('a', 'd', 5), ('b', 'c', 4), ('b', 'g', 7), ('c', 'd', 9), ('c', 'e', 4), ('c', 'g', 3), ('d', 'e', 7), ('e', 'f', 5), ('e', 'g', 2), ('f', 'g', 2)]
+-- "acb"
+optimumPath :: Ord a => a -> a -> [(a, a, Int)] -> [a]
+optimumPath src dest xs
+  | src == dest = []
+  | exists src && exists dest = 
+      let (v, m) = buildDataStr xs
+          [srcIdx, destIdx] = (`V.elemIndex` v) <$> [src, dest]
+          maybePath = join $ index (floydWarshall m) <$> srcIdx <*> destIdx in
+          maybe [] (\p -> src : [v V.! x| (x, _) <- p]) maybePath
+  | otherwise = []
+    where exists src = any (\(x1, x2, _) -> src `elem` [x1, x2]) xs
+
+buildDataStr :: Ord a => [(a, a, Int)] -> (V.Vector a, Matrix)
+buildDataStr xs = let (v, m) = recur xs V.empty M.empty in (v, buildMatrix v m)
+  where recur [] v m = (v, m)
+        recur ((x1, x2, cost) : xs) v m =
+          let newV = updateV x1 $ updateV x2 v
+              newM = M.insert (x2, x1) cost $ M.insert (x1, x2) cost m in
+          recur xs newV newM
+
+        updateV x v
+          | x `elem` v = v
+          | otherwise = V.cons x v
+
+buildMatrix :: Ord a => V.Vector a -> M.Map (a, a) Int -> Matrix
+buildMatrix v m = let seqNums = V.fromList [0..(length v - 1)] in
+                  (\r -> buildCol r <$> seqNums) <$> seqNums
+  where buildCol r c 
+          | r == c = Nothing
+          | otherwise = let k = (v V.! r, v V.! c) in return . (c,) <$> M.lookup k m
+
+floydWarshall ::  Matrix -> Matrix
+floydWarshall matrix = let seqNums = V.fromList [0..(length matrix - 1)] in
+                       foldl (\m k -> (\r -> updateCol m k r <$> seqNums) <$> seqNums) matrix seqNums
+  where updateCol m k r c 
+          | k == r || k == c || r == c = index m r c
+          | otherwise = let origPath = index m r c
+                            rkPath = index m r k
+                            kcPath = index m k c in
+                        takeMin origPath $ rkPath >>= ((<$> kcPath) . (++))
+
+        takeMin Nothing p = p
+        takeMin p Nothing = p
+        takeMin (Just p1) (Just p2) = if costs p1 < costs p2 then Just p1 else Just p2
+
+index :: Matrix -> Int -> Int -> Maybe [(Int, Int)]
+index m r c = (m V.! r ) V.! c
