ea1a476efd3db682bf81b031e0ae9a06dc2e8b8a
jinilover <columbawong@gmail.com>
2019-03-15 23:27:06 +0800

    add RoseTrees.hs

--- src/Graphs.hs
+++ src/Graphs.hs
@@ -9,19 +9,6 @@ import Data.List (nub)
 import qualified Data.Map as M
 import qualified Data.Vector as V
 
-data RoseTree a = Nde a [RoseTree a] deriving Show -- is it n-ary tree can be presented by cofree
-
-foldRoseTree :: (a -> [b] -> b) -> b -> RoseTree a -> b
-foldRoseTree f y (Nde x branches) = f x $ map (foldRoseTree f y) branches
-
-data Free f a = Pure a
-              | Free f (Free f a)
-
-data Cofree f a =  a :< f (Cofree f a)
-
-foldCofree :: Functor f => (a -> f b -> b) -> b -> Cofree f a -> b
-foldCofree g b (x :< fOfCofree) = g x (foldCofree g b <$> fOfCofree)
-
 data Node a = Node {
     node :: a
   , adjacents :: [a]
